ccb40ff9d287f3c798580f908fe0bbc063956d2d
BlonRed <balf,dw@gmail.com>
2023-08-08 21:56:08 +0400

    Initial commit

--- src/main/java/animals/Kotik.java
+++ src/main/java/animals/Kotik.java
@@ -53,7 +53,7 @@ public class Kotik {
     }
 
     private void eat() {
-        this.eat(4, "курица");
+        this.eat(3, "курица");
     }
 
     synchronized private boolean actionProcessed(String action) {
